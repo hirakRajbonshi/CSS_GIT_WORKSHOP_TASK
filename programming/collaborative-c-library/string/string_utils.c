cdad7e836bcf37d45b19190762ac10344089bd4a
Eddie <adhyax626@gmail.com>
2025-01-26 15:37:52 +0530

    updates made

--- programming/collaborative-c-library/string/string_utils.c
+++ programming/collaborative-c-library/string/string_utils.c
@@ -155,13 +155,43 @@ int count_number_of_occurrences(const char* str, const char* sub) {
     return count;
 }
 
+int is_valid_ip_address(const char* str) {
+    if (str == NULL) return 0;
 
+    int dot_count = 0;  
+    int num_count = 0;  
+    int segment_value = 0;  
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+
+        if (isdigit(str[i])) {
+
+            segment_value = segment_value * 10 + (str[i] - '0');
+            num_count++;
+        }
+        
+        else if (str[i] == '.') {
+            if (num_count == 0) return 0;
+            if (segment_value < 0 || segment_value > 255) return 0;
+
+            dot_count++;  
+            segment_value = 0; 
+            num_count = 0;  
+        } else {
+            return 0;
+        }
+    }
+
+    if (num_count == 0 || segment_value < 0 || segment_value > 255) return 0;
+
+    return dot_count == 3;
+}
 
 
 int main() {
 
     char str1[100];
-    printf("Enter a string to reverse: ");
+    printf("Enter a string to reverse: ");  //reverse string
     fgets(str1, sizeof(str1), stdin);  
     str1[strcspn(str1, "\n")] = '\0';  
     char* reversed_str = reverse_string(str1);
@@ -175,7 +205,7 @@ int main() {
 
 
 
-    char str2[100];
+    char str2[100];     //uppercase
     printf("Enter a string to convert to uppercase: ");
     fgets(str2, sizeof(str2), stdin);  
     str2[strcspn(str2, "\n")] = '\0';  
@@ -190,7 +220,7 @@ int main() {
 
 
 
-    char str3[100];
+    char str3[100];     //lowercase
     printf("Enter a string to convert to lowercase: ");
     fgets(str3, sizeof(str3), stdin);  
     str3[strcspn(str3, "\n")] = '\0';  
@@ -205,7 +235,7 @@ int main() {
 
 
 
-    char str4[100], sub4[50];
+    char str4[100], sub4[50];       //find substring
     printf("Enter the main string to find the substring of: ");
     fgets(str4, sizeof(str4), stdin);
     str4[strcspn(str4, "\n")] = '\0';
@@ -222,7 +252,7 @@ int main() {
 
 
 
-    char str5[100];
+    char str5[100];     //count vowels
     printf("Enter a string to count the vowels: ");
     fgets(str5, sizeof(str5), stdin);
     str5[strcspn(str5, "\n")] = '\0'; 
@@ -231,7 +261,7 @@ int main() {
 
 
 
-    char str6[100];
+    char str6[100];     //count consonants
     printf("Enter a string to count the consonants: ");
     fgets(str6, sizeof(str6), stdin);
     str6[strcspn(str6, "\n")] = '\0';  
@@ -240,7 +270,7 @@ int main() {
 
 
 
-    char str7[100];
+    char str7[100];     //count words
     printf("Enter a string to count words: ");
     fgets(str7, sizeof(str7), stdin);
     str7[strcspn(str7, "\n")] = '\0';  
@@ -249,7 +279,7 @@ int main() {
 
 
 
-    char str8[200];
+    char str8[200];     //count sentences
     printf("Enter a string to count sentences (use proper grammar): ");
     fgets(str8, sizeof(str8), stdin);
     str8[strcspn(str8, "\n")] = '\0'; 
@@ -258,7 +288,7 @@ int main() {
 
 
 
-    char str9[200], sub9[100];
+    char str9[200], sub9[100];      //count occurences
     printf("Enter the main string: ");
     fgets(str9, sizeof(str9), stdin);
     str9[strcspn(str9, "\n")] = '\0';  
@@ -269,6 +299,17 @@ int main() {
     printf("Number of occurrences: %d\n", occurrences);
 
 
+
+    char ip_address[20];        //valid IP address
+    printf("Enter an IP address: ");
+    fgets(ip_address, sizeof(ip_address), stdin);
+    ip_address[strcspn(ip_address, "\n")] = '\0'; 
+
+    if (is_valid_ip_address(ip_address)) {
+        printf("The IP address is valid.\n");
+    } else {
+        printf("The IP address is not valid.\n");
+    }
 
 
     return 0;
