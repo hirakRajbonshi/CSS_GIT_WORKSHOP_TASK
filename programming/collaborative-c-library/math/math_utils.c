4ae761e05829552d446d81c78c7c7a8334abf7fc
Sarania-M <facebookmadhurjya@gmail.com>
2025-01-27 23:57:12 +0530

    madhurjya added changes final

--- programming/collaborative-c-library/math/math_utils.c
+++ programming/collaborative-c-library/math/math_utils.c
@@ -1,5 +1,10 @@
 #include "math_utils.h"
+#include<stdio.h>
 #include<math.h>
+
+double square_root(double n);
+double cube_root(double n);
+double nth_root(double n, int root);
 
 int factorial(int n) {
     if (n == 0) {
@@ -34,17 +39,45 @@ double cube_root(double n) {
 }
 
 
-double nth_root(double n, int root) {
-    if (root < 1) {
-        return -1;
+double nth_root(double base, int n) {
+    return pow(base, 1.0 / n);
+}
+
+
+int main() {
+    double num;
+
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+
+    double sqrt_result = square_root(num);
+    double cbrt_result = cube_root(num);
+     
+
+    printf("Square root of %.2f is: %.6f\n", num, sqrt_result);
+    printf("Cube root of %.2f is: %.6f\n", num, cbrt_result);
+    
+    double base;
+    int n;
+
+    
+    printf("Enter the number: ");
+    scanf("%lf", &base);
+
+    printf("Enter the value of n (root): ");
+    scanf("%d", &n);
+
+    
+    if (n <= 0) {
+        printf("The value of n must be greater than 0.\n");
+    } else if (base < 0 && n % 2 == 0) {
+        printf("For even roots, the base must be non-negative.\n");
+    } else {
+       
+        double result = nth_root(base, n);
+        printf("The %.d-th root of %.2lf is %.5lf\n", n, base, result);
     }
-    
-    double y = n;
-    double epsilon = 0.000001;
-    
-    while (fabs(pow(y, root) - n) > epsilon) {
-        y = ((root - 1) * y + n / pow(y, root - 1)) / root;
-    }
-    
-    return y;
+
+    return 0;
+   
 }
