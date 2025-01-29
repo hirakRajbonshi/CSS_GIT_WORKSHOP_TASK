91c0263d940fe9bbe5ff27e98d34093344b6ecda
Akshayy-singh <heyh92877@gmail.com>
2025-01-29 14:42:56 +0530

    Added 4,25,26 of geometry

--- programming/collaborative-c-library/geometry/geometry_utils.c
+++ programming/collaborative-c-library/geometry/geometry_utils.c
@@ -1,3 +1,21 @@
 #include "geometry_utils.h"
+#include<math.h>
 #define PI 3.14159
 
+double area_circle(double radius) {
+    return PI * radius * radius; 
+}
+
+double triangle_area(double x1, double y1, double x2, double y2, double x3, double y3) {
+    return 0.5 * fabs(x1 * y2 + x2 * y3 + x3 * y1 - (y1 * x2 + y2 * x3 + y3 * x1));
+}
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+double perimeter_triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+    double side1 = distance(x1, y1, x2, y2);
+    double side2 = distance(x2, y2, x3, y3);
+    double side3 = distance(x3, y3, x1, y1);
+    return side1 + side2 + side3;
+}
--- programming/collaborative-c-library/main.c
+++ programming/collaborative-c-library/main.c
@@ -7,10 +7,13 @@ int main() {
     printf("The gcd of 24 and 96 is: %d\n", gcd(24,96));
     printf("The LCM of 90 and 40 is: %d\n",lcm(90,40));
     printf("2 raised to the power 6 is: %lf\n",power(2.0,6));
-    printf("the square root of 31 is: %lf\n",square_root(31.0));
+    printf("The square root of 31 is: %lf\n",square_root(31.0));
     printf("The cube root of 36 is: %lf\n",cube_root(36.0));
     printf("Enter the value of n to find the nth root of 46: ");
     scanf("%d",&n);
     printf("The %dth root of 46 is %lf",n,nth_root(46.0,n));
+    printf("The area of circle with radius 7.2 is %lf\n",area_circle(7.2));
+    printf("The area of  triangle with co-ordinates (1,0),(3.3,-8.4),(0,5) is: %lf\n",triangle_area(1.0,0.0,3.3,-8.4,0,5));
+    printf("The perimeter of triangle with co-ordinates (1,0),(3.3,-8.4),(0,5) is: %lf\n",perimeter_triangle(1.0,0.0,3.3,-8.4,0,5));
     return 0;
 }
